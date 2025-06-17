46eb31e53a807d8f46583f48bca8e2342b3179d7
brunols7 <bruno.lsilva1508@gmail.com>
2025-06-17 13:24:54 -0300

    Hotfix precision and scale

--- src/main/java/me/brunols/api_restful_java/model/Account.java
+++ src/main/java/me/brunols/api_restful_java/model/Account.java
@@ -22,10 +22,10 @@ public class Account {
     @NotNull
     private String agency;
 
-    @Column(precision = 2, scale = 13)
+    @Column(scale = 2, precision = 13)
     private BigDecimal balance;
 
-    @Column(name="additional_limit",precision = 2, scale = 13)
+    @Column(name="additional_limit",scale = 2, precision = 13)
     private BigDecimal limit;
 
 }
