194d55a0a4ebb33fef7822fb22c71e273dbdcad0
brunols7 <bruno.lsilva1508@gmail.com>
2025-06-17 12:34:33 -0300

    Setup

---
+++ src/test/java/me/brunols/api_restful_java/ApiRestfulJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.brunols.api_restful_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
